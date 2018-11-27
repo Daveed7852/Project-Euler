9d4e4d21ea8a9ad9c54d37355b64823d5f4fbdf5
David Birkenmeier <dbirkenmeier@gmail.com>
2018-11-26 20:17:59 -0600

    Cleaning code

--- Project5/src/Project5.java
+++ Project5/src/Project5.java
@@ -1,53 +1,22 @@
 public class Project5 {
-    public static void main(String [] args){
-        for(int i=1;i<=1000000000;i++){
-            if(i%1==0) {
-                if (i % 2 == 0) {
-                    if (i % 3 == 0) {
-                        if (i % 4 == 0) {
-                            if (i % 5 == 0) {
-                                if (i % 7 == 0) {
-                                    if (i % 8 == 0) {
-                                        if (i % 9 == 0) {
-                                            if (i % 9 == 0) {
-                                                if (i % 10 == 0) {
-                                                    if(i%11==0){
-                                                        if(i%12==0){
-                                                            if(i%13==0){
-                                                                if(i%14==0){
-                                                                    if(i%15==0){
-                                                                        if(i%16==0){
-                                                                            if(i%17==0){
-                                                                                if(i%18==0){
-                                                                                    if(i%19==0){
-                                                                                        if(i%20==0){
-                                                                                            System.out.print(i);
-                                                                                            break;
-                                                                                        }
-                                                                                    }
-                                                                                }
-                                                                            }
-                                                                        }
 
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
+    private static Boolean checkIsDivisible(int number, int divisor) {
+        if (number % divisor == 0) {
+            return divisor == 20 ? Boolean.TRUE : checkIsDivisible(number, divisor + 1);
+        } else {
+            return Boolean.FALSE;
+        }
+    }
 
-                                                    }
-
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1000000000; i++) {
+            if (checkIsDivisible(i, 1)) {
+                System.out.println(i);
+                break;
             }
-            else continue;
         }
 
     }
+
+    // 232792560
 }
